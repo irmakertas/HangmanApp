fa8fba2488393b4f8b8ec72c1575b73a0fc5ded5
Irmak Ertaş <irmakertas8@gmail.com>
2025-01-08 23:31:37 +0300

    Initial commit for the Hangman game project
    This commit includes the implementation of a simple Hangman game in Java. The project contains two main classes (GameService and HangmanApp) and a text file with word samples.

---
+++ HangmanApp.java
@@ -0,0 +1,12 @@
+package hangmanApp;
+
+import java.io.FileNotFoundException;
+
+public class HangmanApp {
+    public static void main(String[] args) throws FileNotFoundException {
+        GameService gameService = new GameService();
+        gameService.start();
+
+
+    }
+}
